0c29139c19847c02302eba71aa27a8662781571e
maxmiles <maxmiles@uw.edu>
2021-02-05 21:42:46 -0800

    updated about us

--- analysis.R
+++ analysis.R
@@ -7,9 +7,3 @@ library(quantmod)
 content <- read_html("http://web.archive.org/web/20210131152856/http://gambiste.com/index.php/gambiste-daily-stock-rating/")
 tables <- content %>% html_table(fill = TRUE)
 df <- do.call(rbind.data.frame, tables)
-
-<<<<<<< HEAD
-# Scatter <- 
-=======
-
->>>>>>> 76760978f265974cddc11dcf5dd125a137642587
--- app_ui.R
+++ app_ui.R
@@ -13,13 +13,25 @@ home_page <- tabPanel(
 )
 
 page_two <- tabPanel(
+  "Mission Statement",
   sidebarLayout(
     sidebarPanel(
-      p("t")
+      h1("About Us:"),
+      p("We are Augene Pak, Dhruv Karia, Justin Zeng, and Max Bennett. We are
+        Informatics majors in the University of Washington and are each a member
+        of the Information School's class of 2024.")
     ),
     mainPanel(
-      h1("title")
-    )
+      h1("Our Mission:"),
+      p("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.")
+    ),
+    position = "right"
   )
 )
 
@@ -35,6 +47,7 @@ ui <- fluidPage(
       margins = c(40, 20),
       height = "5%",
       width = "5%",
-      position = "bottom-right"
+      position = "bottom-right",
+      timeout = 50
   )
 )
