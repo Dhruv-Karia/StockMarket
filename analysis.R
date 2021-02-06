b43336e8b57c03d3c9fa4bb91e88f2662c504f6f
Dhruv Karia <dhruvkaria@hotmail.com>
2021-02-05 20:57:29 -0800

    try 1

--- analysis.R
+++ analysis.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 library(rvest)
 library(BatchGetSymbols)
-
-content <- read_html("http://gambiste.com/")
-
+library(quantmod)
+content <- read_html("http://web.archive.org/web/20210131152856/http://gambiste.com/index.php/gambiste-daily-stock-rating/")
 tables <- content %>% html_table(fill = TRUE)
 df <- do.call(rbind.data.frame, tables)
+
+
+
+
+
+
 
 
