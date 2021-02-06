d234db00467873dabd08eaaf2f75a2cb0e27c2e3
jzenguw <justinzeng007@gmail.com>
2021-02-05 16:05:22 -0800

    home page 压岁é added

--- analysis.R
+++ analysis.R
@@ -1,10 +1,11 @@
 library(tidyverse)
 library(rvest)
 library(BatchGetSymbols)
+library(plotly)
 
 content <- read_html("http://gambiste.com/")
 
 tables <- content %>% html_table(fill = TRUE)
 df <- do.call(rbind.data.frame, tables)
 
-
+Scatter <- 
--- app_ui.R
+++ app_ui.R
@@ -1,4 +1,23 @@
-page_one <- tabPanel()
+source("analysis.R")
+
+home_page <- tabPanel(
+  sidebarLayout(
+    "Top Recent Stock Prices",
+    sidebarPanel(
+      sliderInput(
+        inputId = "Week",
+        label = "Week Slider",
+        min = 0,
+        max = 52,
+        value = 52
+      ),
+    ),
+    mainPanel(
+      "title"
+     # plotOutput(outputId = "")
+    )
+  )
+)
 
 page_two <- tabPanel(
   sidebarLayout(
