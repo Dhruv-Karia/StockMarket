383bad032c8fa0d4af063f0cfb5b1addb550c58a
maxmiles <maxmiles@uw.edu>
2021-02-06 22:08:18 -0800

    merge changes

--- app.R
+++ app.R
@@ -11,7 +11,19 @@ library(DT)
 library(newsanchor)
 library(jsonlite)
 library(httr)
+library(RedditExtractoR)
+library(gganimate)
+library(gifski)
+library(png)
+library(plotly)
 
+# Source UI and server
+source("app_ui.R")
+source("app_server.R")
+
+# Run shiny app
+# thematic::thematic_shiny()
+shinyApp(ui = ui, server = server)
 # Source UI and server
 source("app_ui.R")
 source("app_server.R")
